3d797537ed0f4cf925bc0e081c9ee6a5a9c8950e
mahnafdev <mahnafdev@gmail.com>
2025-11-16 23:08:25 +0600

    feat(coding): solve problem-3

--- solutions.ts
+++ solutions.ts
@@ -10,3 +10,15 @@ const getLength = <T>(iterable: T): number | undefined => {
 	if (typeof iterable === "string") return iterable.length;
 	else if (Array.isArray(iterable)) return iterable.length;
 };
+
+class Person {
+	name: string;
+	age: number;
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+	getDetails() {
+		return `'Name: ${this.name}, Age: ${this.age}'`;
+	}
+}
