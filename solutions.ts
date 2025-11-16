0b8055833bb2e05c112d1dfe06004e1f05b124ad
mahnafdev <mahnafdev@gmail.com>
2025-11-16 21:46:54 +0600

    feat(coding): solve problem-2

--- solutions.ts
+++ solutions.ts
@@ -5,3 +5,8 @@ const formatValue = (
 	else if (typeof value === "number") return value * 10;
 	else if (typeof value === "boolean") return !value;
 };
+
+const getLength = <T>(iterable: T): number | undefined => {
+	if (typeof iterable === "string") return iterable.length;
+	else if (Array.isArray(iterable)) return iterable.length;
+};
